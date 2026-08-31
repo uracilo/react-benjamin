a7cbcd98f3e591a5c15bbef3a6775b7e55dc36f6
benjamin <benjamin.casazza@gmail.com>
2026-08-31 13:05:04 -0600

    feature rick and morty

--- practica_rick_morty/functions.js
+++ practica_rick_morty/functions.js
@@ -1,1 +1,29 @@
-console.log("Hello World");
+console.log("Hello World Rick and Morty");
+
+async function obtenerPersonajes() {
+    const response = await fetch("https://rickandmortyapi.com/api/character");
+    const data = await response.json();
+    console.log('Personajes:', data.results);
+    return data.results;
+}
+
+console.log(obtenerPersonajes());
+
+function pintarPersonajes(personajes) {
+    console.log("Pintando personajes:", personajes);
+    let tarjetasHTML = "";
+    console.log("Tarjetas HTML:", tarjetasHTML);
+    personajes.forEach(personaje => {
+        tarjetasHTML += `
+        <div class="card">
+            <img src="${personaje.image}" alt="${personaje.name}">
+            <h3>${personaje.name}</h3>
+            <p class="species">${personaje.species}</p>
+            <p class="status">${personaje.status}</p>
+        </div>
+        `;
+    });
+    document.getElementById("main-container").innerHTML = tarjetasHTML;
+}
+
+obtenerPersonajes().then(pintarPersonajes);
